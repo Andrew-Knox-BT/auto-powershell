6978d482737bc5585c02d21c10a7918e777f082b
Andrew Knox <aknox@beyondtrust.com>
2022-07-12 10:51:43 -0300

    update

--- UVMRunLocal.ps1
+++ UVMRunLocal.ps1
@@ -1,5 +1,12 @@
-Set-NetFirewallRule -PolicyStore localhost -DisplayName 'TCP 445( SMB )' -Enabled True
+#Create Firewall rule to open port 5985 for WIN RM
+#This will create the firewall rule in the local gpo
+New-NetFirewallRule -PolicyStore localhost -DisplayName "Windows Remote Management (HTTP-In) - For Automation" -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow -Profile Public -Enabled True -RemoteAddress Any -LocalAddress Any -RemotePort Any 
 
-Invoke-CimMethod -CimSession $cimsession -ClassName Win32_Process -MethodName "Create" -Arguments @{
-    CommandLine = 'gpupdate.exe'; CurrentDirectory = "C:\windows\system32"
-  }
+
+
+#After the gpo is updated need to run gpupdate to apply the new changes. 
+Invoke-CimMethod -ClassName Win32_Process -MethodName "Create" -Arguments @{
+  CommandLine = 'gpupdate.exe'; CurrentDirectory = "C:\windows\system32"
+}
+
+
