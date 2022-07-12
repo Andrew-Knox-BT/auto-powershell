889da16ab33d657b2791c0441bc6c4096766e063
Andrew Knox <aknox@beyondtrust.com>
2022-07-12 12:13:18 -0300

    update

--- UVMRunLocal.ps1
+++ UVMRunLocal.ps1
@@ -1,15 +1,13 @@
 #Create Firewall rule to open port 5985 for WIN RM
 #This will create the firewall rule in the local gpo
+
 #TODO - Add a check that is the rule exists skip this step
 New-NetFirewallRule -PolicyStore localhost -DisplayName "Escalation Automation - Windows Remote Management (HTTP-In)" -Direction Inbound -LocalPort 5985 -Protocol TCP -Action Allow -Profile Public -Enabled True -RemoteAddress Any -LocalAddress Any -RemotePort Any 
 
 
 #TODO - If previous rule exists check the activestore to see if the rule exists and that it's enabled. If so skip the gpupdate. 
 
-write-host "Localhost"
-Get-NetFirewallRule  -PolicyStore localhost | Where-Object {$_.displayname -like 'Escalation Automation*'} | Format-List DisplayName, Enabled
-write-host "ActiveStore"
-Get-NetFirewallRule  -PolicyStore ActiveStore | Where-Object {$_.displayname -like 'Escalation Automation*'} | Format-List DisplayName, Enabled
+
 
 <#
 #Only use when firewall rules need to be removed and cleaned up. 
@@ -22,8 +20,4 @@ Invoke-CimMethod -ClassName Win32_Process -MethodName "Create" -Arguments @{
   CommandLine = 'gpupdate.exe'; CurrentDirectory = "C:\windows\system32"
 }
 
-write-host "Localhost"
-Get-NetFirewallRule  -PolicyStore localhost | Where-Object {$_.displayname -like 'Escalation Automation*'} | Format-List DisplayName, Enabled
-write-host "ActiveStore"
-Get-NetFirewallRule  -PolicyStore ActiveStore | Where-Object {$_.displayname -like 'Escalation Automation*'} | Format-List DisplayName, Enabled
-
+Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\system\' -Name 'LocalAccountTokenFilterPolicy' -Value 1
