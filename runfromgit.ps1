d5ca65db736c046dc93b79702637122aad987436
Andrew Knox <aknox@beyondtrust.com>
2022-07-12 11:12:40 -0300

    changed name of firewall rule

--- runfromgit.ps1
+++ runfromgit.ps1
@@ -1,3 +1,3 @@
-$ScriptFromGitHub = Invoke-WebRequest https://github.com/Andrew-Knox-BT/auto-powershell/blob/main/UVMRunLocal.ps1
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Andrew-Knox-BT/auto-powershell/main/UVMRunLocal.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
 
