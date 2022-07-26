c4f02b1c660dbd467f253a0a054917e60de6bb29
Andrew Knox <aknox@beyondtrust.com>
2022-07-26 10:14:13 -0300

    updates

--- UVMRunLocal/UVMRunLocal.ps1
+++ UVMRunLocal/UVMRunLocal.ps1
@@ -29,22 +29,21 @@ $allips = Get-NetIPAddress | Select-Object -ExpandProperty IPv4Address
 foreach ($ip in $allips) {
     if ($ip -like '10.200.113.*' -OR $ip -like '10.200.114*' ) { 
        
-       $interfacealias = Get-NetIPAddress | Where-Object ipv4address -eq $ip | Select-Object -ExpandProperty InterfaceAlias
+        $interfacealias = Get-NetIPAddress | Where-Object ipv4address -eq $ip | Select-Object -ExpandProperty InterfaceAlias
+        #To make the servers easier to manage they need to be added to DNS
+        #Set DNS Address. Currently in lab the DNS server is 10.200.114.105
+        Set-DnsClientServerAddress -InterfaceAlias $interfacealias -ServerAddresses ("10.200.114.105")
+
+        #Set DNS suffix for this connection and Set Use this connections DNS suffix in DNS registration
+        #Curretly in lab the main domain for UVM's is - uvm.lab
+        set-dnsclient -InterfaceAlias $interfacealias -ConnectionSpecificSuffix 'uvm.lab' -UseSuffixWhenRegistering $true
+        #With this setting it will use the DNS suffix specified in the DNS suffix for this connection box to regeister the machein in DNS
+
+        #Run command to register the server in DNS
+        Register-DnsClient 
     }
     else {
-        Write-Host 'no ip found'
+        Write-Output "No Matching IP address found"
     }
+   
 }
-
-#To make the servers easier to manage they need to be added to DNS
-#Set DNS Address. Currently in lab the DNS server is 10.200.114.105
-Set-DnsClientServerAddress -InterfaceAlias $interfacealias -ServerAddresses ("10.200.114.105")
-
-#Set DNS suffix for this connection and Set Use this connections DNS suffix in DNS registration
-#Curretly in lab the main domain for UVM's is - uvm.lab
-set-dnsclient -InterfaceAlias $interfacealias -ConnectionSpecificSuffix 'uvm.lab' -UseSuffixWhenRegistering $true
-#With this setting it will use the DNS suffix specified in the DNS suffix for this connection box to regeister the machein in DNS
-
-#Run command to register the server in DNS
-Register-DnsClient 
-
